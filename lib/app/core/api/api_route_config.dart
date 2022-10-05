74eb8f2e86fb3183889ba8dcf8c44990d01da0e2
DanielAviiles <srhackme@gmail.com>
2022-10-05 16:09:30 -0500

    feat(Root): change configuration api config

--- lib/app/core/api/api_route_config.dart
+++ lib/app/core/api/api_route_config.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_marvel_dev/app/core/utils/hash_calculator.dart';
 
 class ApiRouteConfig {
   static String get baseUrl => dotenv.get('BASE_URL', fallback: null);
-  static String get apiKey => dotenv.get('API_KEY', fallback: null);
-  static String get baseImgUrl => dotenv.get('BASE_IMAGE_URL', fallback: null);
-  static String get language => dotenv.get('LANGUAGE', fallback: null);
+  static String get publicApiKey =>
+      dotenv.get('PUBLIC_API_KEY', fallback: null);
+  static String get privateApiKey =>
+      dotenv.get('PRIVATE_API_KEY', fallback: null);
+  static String get hashConf =>
+      CodeCalculator(publicApiKey: publicApiKey, privateApiKey: privateApiKey)
+          .code;
   static Future<void> loadEnviroments() async {
     if (kReleaseMode) {
       await dotenv.load(fileName: '.env.production');
